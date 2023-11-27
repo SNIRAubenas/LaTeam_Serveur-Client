ae8a7caa476a7b74dfe0b361f0d7fa1d1b9bab11
ghongnard <guillaume.hongnard@lyceeastier.com>
2023-11-27 14:07:58 +0100

    Add project files.

---
+++ ServeurMessagerie/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ServeurMessagerie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
