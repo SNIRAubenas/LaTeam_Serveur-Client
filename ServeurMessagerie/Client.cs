fe12f27b4c3e96adf0a938d3c29198fa28e3b8b0
ncornuel <nino.cornuel@lyceeastier.com>
2023-11-27 15:08:39 +0100

    client

--- ServeurMessagerie/Client.cs
+++ ServeurMessagerie/Client.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ServeurMessagerie
 {
-    internal class Client
+    class Client
     {
+        private TcpClient client;
+        private NetworkStream stream;
+        private Thread thread;
+
+        public Client(TcpClient client)
+        {
+            this.client = client;
+            this.stream = client.GetStream();
+        }
+
+        public void Start()
+        {
+            this.thread = new Thread(this.threadStart);
+            this.thread.Start();
+        }
+
+        void threadStart(object? obj)
+        {
+            byte[] buffer = new byte[2048];
+
+            do
+            {
+                try
+                {
+                    int read = stream.Read(buffer, 0, buffer.Length);
+                    stream.Write(buffer, 0, read);
+                }
+                catch
+                {
+                    break;
+                }
+
+            } while (true);
+
+        }
+
+        public void Stop() 
+        {
+            this.client.Close();
+        }
     }
 }
--- ServeurMessagerie/Serveur.cs
+++ ServeurMessagerie/Serveur.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ServeurMessagerie
+
 {
     internal class Serveur
     {
+
+        private IPEndPoint ep;
+        private TcpListener listener;
+        private Thread th;
+        private List<EchoWork> clients;
+
     }
 }
